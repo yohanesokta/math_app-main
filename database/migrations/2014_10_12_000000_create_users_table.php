c9760842480969a7d7b478d8df8cba2d798db8ba
yohanesokta <yohanesoktayt72@gmail.com>
2023-11-06 09:24:48 +0700

    aku pusing ganti db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('guruTokens')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
